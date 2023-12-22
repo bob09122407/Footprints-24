6e8b828761c15335a5fc437f795e4b9402d09907
karm1000 <karmdsoni8159@gmail.com>
2023-12-23 01:16:00 +0530

    Timeline structure complete

--- src/components/HistoryPage/HistoryPage.jsx
+++ src/components/HistoryPage/HistoryPage.jsx
@@ -4,97 +4,47 @@ import Heading from "../Common/Headings/Heading";
 import "./HistoryPage.css";
 
 function HistoryPage() {
-
   useEffect(() => {
-  
-    document.title="History | FootPrints'23"
+    document.title = "History | FootPrints'23";
+  }, []);
 
-  }, [])
+  useEffect(()=>{
 
-  useEffect(() => {
+    (function($){
+      let prevScrollY = 0;
+      let progressBar =  $(".ag-timeline_line-progress");
+      let branches = $(".branch"); 
+      $(window).on("scroll",function(){
+        //For Progress Bar
+        let currentScrollY = $(this).scrollTop();
 
-    let agPosY;
-    let agHeight;
-    let agTop;
-
-    (function ($) {
-      $(function () {
-
-
-        $(window).on('scroll', function () {
-          fnOnScroll();
-        });
-
-        $(window).on('resize', function () {
-          fnOnResize();
-        });
-
-
-        let agTimeline = $('.js-timeline'),
-          agTimelineLine = $('.js-timeline_line'),
-          agTimelineLineProgress = $('.js-timeline_line-progress'),
-          agTimelinePoint = $('.js-timeline-card_point-box'),
-          agTimelineItem = $('.js-timeline_item'),
-          agOuterHeight = $(window).outerHeight(),
-
-          f = -1,
-          agFlag = false;
-
-        agHeight = $(window).height();
-
-        function fnOnScroll() {
-
-          agPosY = $(window).scrollTop();
-
-          fnUpdateFrame();
+        if(currentScrollY > prevScrollY){
+          progressBar.height(progressBar.height()+(currentScrollY-prevScrollY));
+        }else{
+          progressBar.height(progressBar.height()-(prevScrollY-currentScrollY));
         }
 
-        function fnOnResize() {
-          agPosY = $(window).scrollTop();
-          agHeight = $(window).height();
+        prevScrollY = currentScrollY;
+        
+        // For Branch
+        branches.each(function(index,branch){
+          // console.log(progressBar.offset().top+progressBar.height(), branch.offsetTop);
+          if((progressBar.offset().top+progressBar.height()) >= $(branch).offset().top){
+            // $(branch).find("path").animate({
+            //   strokeDasharray:250
+            // },"fast");
+            $(branch).find("path").addClass("path-animate");
+          }else{
+            $(branch).find("path").removeClass("path-animate");
+          }
+        })
 
-          fnUpdateFrame();
-        }
-
-        function fnUpdateWindow() {
-          agFlag = false;
-
-          agTimelineLine.css({
-            top: agTimelineItem.first().find(agTimelinePoint).offset().top - agTimelineItem.first().offset().top,
-            bottom: agTimeline.offset().top + agTimeline.outerHeight() - agTimelineItem.last().find(agTimelinePoint).offset().top
-          });
-
-          f !== agPosY && (f = agPosY, agHeight, fnUpdateProgress());
-        }
-
-        function fnUpdateProgress() {
-          agTop = agTimelineItem.last().find(agTimelinePoint).offset().top;
-
-          let i = agTop + agPosY - $(window).scrollTop();
-          let a = agTimelineLineProgress.offset().top + agPosY - $(window).scrollTop();
-          let n = agPosY - a + agOuterHeight / 2;
-          i <= agPosY + agOuterHeight / 2 && (n = i - a);
-          agTimelineLineProgress.css({ height: n + "px" });
-
-          agTimelineItem.each(function () {
-            agTop = $(this).find(agTimelinePoint).offset().top;
-
-            (agTop + agPosY - $(window).scrollTop()) < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active');
-          })
-        }
-
-        function fnUpdateFrame() {
-          agFlag || requestAnimationFrame(fnUpdateWindow);
-          agFlag = true;
-        }
+      })
 
 
-      });
+
     })(jQuery);
-
-
-  })
-
+  },[])
 
   return (
     <>
@@ -113,60 +63,40 @@ function HistoryPage() {
                   <div className="js-timeline_line-progress ag-timeline_line-progress"></div>
                 </div>
 
-                <div className="ag-timeline_list">
+                <div className="timeline-list">
                   {HistoryData.map((element, index) => {
                     return (
-                      <div
-                        className="js-timeline_item ag-timeline_item"
-                        key={index}
-                      >
-                        <div className="ag-timeline-card_box">
-                          {(index + 1) % 2 == 1 ? (
-                            <>
-                              <div className="js-timeline-card_point-box ag-timeline-card_point-box">
-                                <div className="ag-timeline-card_point">
-                                  20{element.year}
-                                </div>
-                              </div>
-
-                              <div className="ag-timeline-card_meta-box">
-                                <div className="ag-timeline-card_meta">
-                                  Footprints'{element.year}
-                                </div>
-                              </div>
-                            </>
-                          ) : (
-                            <>
-                              <div className="ag-timeline-card_meta-box">
-                                <div className="ag-timeline-card_meta">
-                                  Footprints'{element.year}
-                                </div>
-                              </div>
-
-                              <div className="js-timeline-card_point-box ag-timeline-card_point-box">
-                                <div className="ag-timeline-card_point">
-                                  20{element.year}
-                                </div>
-                              </div>
-                            </>
-                          )}
-                        </div>
-
-                        <div className="ag-timeline-card_item">
-                          <div className="ag-timeline-card_inner">
-                            <div className="ag-timeline-card_info">
-                              <div className="ag-timeline-card_title">
+                      <div className="timeline-item" key={index}>
+                        <div>
+                          <div className="main-content">
+                            <div className="timeline-header">
+                              <h3 className="timeline-heading">
+                                {element.heading}
+                              </h3>
+                              <div className="timeline-year">
                                 Footprints'{element.year}
                               </div>
-
-                              <div className="ag-timeline-card_desc">
-                                <h3>{element.heading}</h3>
-
-                                <p>{element.description}</p>
-                              </div>
+                            </div>
+                            <div className="timeline-content">
+                              {element.description}
                             </div>
                           </div>
-                          <div className="ag-timeline-card_arrow"></div>
+                          <div className="branch">
+                            <svg
+                              width="202"
+                              height="133"
+                              viewBox="0 0 202 133"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              
+                              <path
+                                d="M1 128.946C144.205 131.963 58.4037 8.0224 201 4"
+                                stroke="white"
+                                stroke-width="3"
+                              />
+                            </svg>
+                          </div>
                         </div>
                       </div>
                     );
