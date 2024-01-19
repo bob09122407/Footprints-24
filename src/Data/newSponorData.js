a388f81baeafba7dc8805571933eb3d599d27122
Krisha <krishahdesai.9@gmail.com>
2024-01-19 13:08:46 +0530

    sponsors

--- src/Data/newSponorData.js
+++ src/Data/newSponorData.js
@@ -7,24 +7,24 @@ const newSponsorData = [
 
     {
         id: 1.1,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370198/NewSponsorFp-23/Matrix_ljqcut.png`,
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648670/sponsor/nqyxxm03say30mlqnw9l.png`,
         alt: `Matrix`,
         href: `https://www.matrixcomsec.com/`,
         type: "TITLE SPONSOR"
     },
 
+    // {
+    //     id: 1.2,
+    //     imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370198/NewSponsorFp-23/GlobalCoallience_r1izgh.png`,
+    //     alt: `Global Colinece`,
+    //     href: `https://globalcolliance.com/`,
+    //     type: "CO-SPONSOR"
+    // },
+
+
     {
         id: 1.2,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370198/NewSponsorFp-23/GlobalCoallience_r1izgh.png`,
-        alt: `Global Colinece`,
-        href: `https://globalcolliance.com/`,
-        type: "CO-SPONSOR"
-    },
-
-
-    {
-        id: 1.3,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370199/NewSponsorFp-23/KYB-ConMat_gseqgv.png`,
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648670/sponsor/ek8dphd7zt7u08jxnvas.png`,
         alt: `KYB`,
         href: `https://conmatindia.com/`,
         type: "ASSOCIATE SPONSOR"
@@ -32,31 +32,31 @@ const newSponsorData = [
 
     /* Other Sponsor - Grid */
 
-    {
-        id: 2.1,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1677359063/NewSponsorFp-23/Linde-TechSponsor_sepcqz.png`,
-        alt: `Linde`,
-        href: `https://www.linde.com/`
-    },
+    // {
+    //     id: 2.1,
+    //     imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1677359063/NewSponsorFp-23/Linde-TechSponsor_sepcqz.png`,
+    //     alt: `Linde`,
+    //     href: `https://www.linde.com/`
+    // },
 
-    {
-        id: 2,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370199/NewSponsorFp-23/Polycab_sb8cdc.png`,
-        alt: `Polycab`,
-        href: `https://polycab.com/`
-    },
+    // {
+    //     id: 2,
+    //     imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370199/NewSponsorFp-23/Polycab_sb8cdc.png`,
+    //     alt: `Polycab`,
+    //     href: `https://polycab.com/`
+    // },
 
 
     {
         id: 3,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370198/NewSponsorFp-23/Axtel_e2lwsv.png`,
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648840/sponsor/gvim37r3m8im4s3lnmnk.png`,
         alt: `Axtel`,
         href: `https://www.axtelindia.com/`
     },
 
     {
         id: 4,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370198/NewSponsorFp-23/Cosmos_ul3fwt.png`,
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648840/sponsor/cedquclpuhhn59qsuwno.png`,
         alt: `Cosmos`,
         href: `https://www.cosmos.in/`
     },
@@ -64,108 +64,109 @@ const newSponsorData = [
 
     {
         id: 5,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370198/NewSponsorFp-23/Acumen_cmcukv.png`,
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648840/sponsor/c0ofhpwjxibcrnplo7xo.png`,
         alt: `Acumen`,
         href: `https://acumenhr.in/`
     },
 
     {
         id: 6,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370199/NewSponsorFp-23/KEECO_kcrpmw.png`,
-        alt: `Keeco`,
-        href: `https://www.keeco.co.in/`
-
-        /* Website Not working */
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648839/sponsor/ha4wgityhfzdobtweya2.png`,
+        alt: `Robowala`,
+        href: `https://robowala.com/`
     },
 
 
     {
         id: 7,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370198/NewSponsorFp-23/ALMAK_fy9ibt.png`,
-        alt: `Almak`,
-        href: `https://almak.in/`
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648839/sponsor/n0bchj1bl18cmk6kl0n9.png`,
+        alt: `Kats`,
+        href: ``
     },
 
     {
         id: 8,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370199/NewSponsorFp-23/Nilamber_wa02vx.png`,
-        alt: `Nilambar`,
-        href: `https://www.nilambergroup.in/`
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648840/sponsor/hsktuawnmnaewndfr6xi.png`,
+        alt: `Mir India`,
+        href: ``
     },
 
     {
         id: 9,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1675347547/NewSponsorFp-23/Survi-Group_ooxg5q.png`,
-        alt: `SurviGrp`,
-        href: ``
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648840/sponsor/jaiosvg5jwrkrkmutcvv.png`,
+        alt: `Prapti group`,
+        href: ``,
+    },
 
-        /* Website Not Exist */
 
+    {
+        id: 9,
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648840/sponsor/wq0xcejgbv5ngqpu3l9e.png`,
+        alt: `Ims`,
+        href: ``,
     },
 
     {
         id: 10,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1675347545/NewSponsorFp-23/FreeBird_Animation_qqtjdp.png`,
-        alt: `Free Bird Animation`,
-        href: `http://www.freebirdanimation.com/`
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648841/sponsor/beatzvslhcygetejydmo.png`,
+        alt: `Arkel`,
+        href: ``,
     },
 
     {
         id: 11,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1674370199/NewSponsorFp-23/Thenix_vuyz1j.png`,
-        alt: `Thenix`,
-        href: `https://instagram.com/thenix_studio?igshid=Yzg5MTU1MDY=`
-
-        /* Social Media Link */
+        imgSrc: `https://res.cloudinary.com/du1tas6pe/image/upload/v1705648841/sponsor/zghtwxgy07wzvnjqvuqx.png`,
+        alt: `RFME`,
+        href: `https://instagram.com/thenix_studio?igshid=Yzg5MTU1MDY=`,
     },
 
-    {
-        id: 12,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1675347546/NewSponsorFp-23/Vihal_ut959g.png`,
-        alt: `Vihal`,
-        href: `http://vihalbuildcon.com/`
+    // {
+    //     id: 12,
+    //     imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1675347546/NewSponsorFp-23/Vihal_ut959g.png`,
+    //     alt: `Vihal`,
+    //     href: `http://vihalbuildcon.com/`
 
-        /* Website Not working */
+    //     /* Website Not working */
 
-    },
+    // },
 
-    {
-        id: 12.1,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1675347545/NewSponsorFp-23/Kanha_o3ycyp.png`,
-        alt: `Kanha`,
-        href: `http://kanhagroup.com/`
-    },
+    // {
+    //     id: 12.1,
+    //     imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1675347545/NewSponsorFp-23/Kanha_o3ycyp.png`,
+    //     alt: `Kanha`,
+    //     href: `http://kanhagroup.com/`
+    // },
 
-    {
-        id: 12.2,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1675347545/NewSponsorFp-23/Clodester_tke4ng.png`,
-        alt: `Cloudester`,
-        href: `https://cloudester.com/`
-    },
+    // {
+    //     id: 12.2,
+    //     imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1675347545/NewSponsorFp-23/Clodester_tke4ng.png`,
+    //     alt: `Cloudester`,
+    //     href: `https://cloudester.com/`
+    // },
 
-    {
-        id: 12.3,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1677361498/NewSponsorFp-23/MSU_Vision_roset0.png`,
-        alt: `MSU-Vision-2020`,
-        href: `https://msuvision2020.org/`
-    },
+    // {
+    //     id: 12.3,
+    //     imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1677361498/NewSponsorFp-23/MSU_Vision_roset0.png`,
+    //     alt: `MSU-Vision-2020`,
+    //     href: `https://msuvision2020.org/`
+    // },
 
     
-    {
-        id: 12.4,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1677515669/NewSponsorFp-23/CAD_Express_rfxt7k.png`,
-        alt: `CAD_Experts`,
-        href: `https://www.justdial.com/Vadodara/Cad-Experts-Near-Amit-Nagar-Circle-Karelibaug/0265PX265-X265-130911174829-S2L3_BZDET`
+    // {
+    //     id: 12.4,
+    //     imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1677515669/NewSponsorFp-23/CAD_Express_rfxt7k.png`,
+    //     alt: `CAD_Experts`,
+    //     href: `https://www.justdial.com/Vadodara/Cad-Experts-Near-Amit-Nagar-Circle-Karelibaug/0265PX265-X265-130911174829-S2L3_BZDET`
     
-        /* External Website */
-    },
+    //     /* External Website */
+    // },
 
-    {
-        id: 12.5,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1677515668/NewSponsorFp-23/SmartCard_qyueos.png`,
-        alt: `Smard CAD`,
-        href: `http://www.smardcad.co.in/`
-    },
+    // {
+    //     id: 12.5,
+    //     imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1677515668/NewSponsorFp-23/SmartCard_qyueos.png`,
+    //     alt: `Smard CAD`,
+    //     href: `http://www.smardcad.co.in/`
+    // },
 
     /* Partner Images */
 
@@ -333,12 +334,7 @@ const newSponsorData = [
     },
 
 
-    {
-        id: 30,
-        imgSrc: `https://res.cloudinary.com/dwdln9iy2/image/upload/v1675347546/NewSponsorFp-23/Prapti-Group_ximmj8.png`,
-        alt: `Prapti group`,
-        href: ``
-    },
+    
 
 ];
 
