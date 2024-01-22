b0d86543aa2c1f0abf869269bc1ca4efeb70896a
karm1000 <karmdsoni8159@gmail.com>
2024-01-22 22:56:21 +0530

    data of taleido updated/ youtube links updated

--- src/Data/Taleido.js
+++ src/Data/Taleido.js
@@ -11,7 +11,7 @@ const TaleidoData = [
 
     content: `Lynn Cominsky, current Project Head at NASA's Education and Public Outreach Group, is an American astrophysicist and Chair of Astronomy and Physics at Sonoma State University. With over 225 research papers, she founded SSU's EdEon STEM Learning, contributing educational materials to NASA, NSF, and the US Department of Education. Cominsky, a recipient of the 2016 Education Prize from the American Astronomical Society, the 2016 Wang Family Excellence Award, and the 2017 Frank J. Malina Education Medal, demonstrates a commitment to advancing both astrophysics and education.`,
 
-    ytlink: "https://youtu.be/HxMQa-1-TLk",
+    ytlink: "https://www.youtube.com/live/wslTkwvykYM?si=j5PPpFMuhMDHGKoB",
   },
   {
     id: 2,
@@ -24,7 +24,7 @@ const TaleidoData = [
       "https://res.cloudinary.com/dwevqwmg7/image/upload/v1705602186/taleido/bl1lkjha9ak1bwcfvioa.jpg",
 
     content:`Iman Ghanizada, the Global Head of Autonomic Security at Google, is a distinguished figure in technology. Recognized in Forbes 30 under 30 for Enterprise and Technology in 2022, he is also a leader at the World CIO Summit 2023. An author of the Google Cloud Certified Professional Cloud Architect, Ghanizada is an investor, startup advisor, and Board Advisor at Virginia Tech University. Co-founder and Board member at Afghan Refugee Relief, he balances technology leadership with philanthropic initiatives, showcasing a versatile and impactful career.`,
-    ytlink: "https://youtu.be/s88Yz2QRSHQ",
+    ytlink: "https://www.youtube.com/live/X7jticXwK30?si=qQn54toFhDdd16gq",
   },
 ];
 
