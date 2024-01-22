06922e5e6d30688a4f1318b81f2160616b7e9249
karm1000 <karmdsoni8159@gmail.com>
2024-01-22 22:48:45 +0530

    taleidoscope youtube links style updated

--- src/components/KaleidoPage/Taleido.jsx
+++ src/components/KaleidoPage/Taleido.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 import TaleidoData from "../../Data/Taleido";
 import Sponsors from "../Common/SponsorSlide/Sponsors";
 import Footer from "../Common/Footer/Footer";
@@ -6,29 +6,30 @@ import "./Taleido.css";
 import Heading from "../Common/Headings/Heading";
 
 function Taleido() {
-
   useEffect(() => {
-  
-    document.title="Teleidoscope | FootPrints'24"
-
-  }, [])
+    document.title = "Teleidoscope | FootPrints'24";
+  }, []);
 
   return (
     <>
       <section id="Taleido">
-      <div className="video_main">
-        <video
-          autoPlay
-          loop
-          muted
-          className="w-full h-full object-cover"
-          width="100%"
-        >
-          <source src={'https://res.cloudinary.com/du1tas6pe/video/upload/v1705679364/vdo/nwhsgnzgd80yqspqfcap.mp4'} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-
+        <div className="video_main">
+          <video
+            autoPlay
+            loop
+            muted
+            className="w-full h-full object-cover"
+            width="100%"
+          >
+            <source
+              src={
+                "https://res.cloudinary.com/du1tas6pe/video/upload/v1705679364/vdo/nwhsgnzgd80yqspqfcap.mp4"
+              }
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </div>
 
         <div className="container">
           <Heading
@@ -42,12 +43,14 @@ function Taleido() {
               return (
                 <div key={element.id}>
                   <div className="taleido_lec content">
-                    <div className="about_taleido about_taleido_para1"
-                    data-aos="fade-up"
-                    data-aos-duration="1000"
-                    // data-aos-delay="100"
-                    data-aos-offset="400"
-                    data-aos-easing="ease-in-sine">
+                    <div
+                      className="about_taleido about_taleido_para1"
+                      data-aos="fade-up"
+                      data-aos-duration="1000"
+                      // data-aos-delay="100"
+                      data-aos-offset="400"
+                      data-aos-easing="ease-in-sine"
+                    >
                       <div className="taleido_heading">
                         <h3 className="taleido_name">
                           {element.name} <br /> {element.details}
@@ -105,10 +108,7 @@ function Taleido() {
             return (
               <div key={element.id}>
                 <div className="taleido_lec">
-                  <div
-                    className="taleido_img"
-                    data-aos="GuestAnimLeft"
-                  >
+                  <div className="taleido_img" data-aos="GuestAnimLeft">
                     <img
                       src={element.imgSrc}
                       className="Taleido-img"
@@ -116,12 +116,14 @@ function Taleido() {
                     />
                   </div>
 
-                  <div className="about_taleido para1" 
-                  data-aos="fade-up"
-                  data-aos-duration="1000"
-                  // data-aos-delay="100"
-                  data-aos-offset="400"
-                  data-aos-easing="ease-in-sine">
+                  <div
+                    className="about_taleido para1"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    // data-aos-delay="100"
+                    data-aos-offset="400"
+                    data-aos-easing="ease-in-sine"
+                  >
                     <div className="taleido_heading">
                       <h3 className="taleido_name">
                         {element.name} <br /> {element.details}
@@ -135,7 +137,7 @@ function Taleido() {
                       className="yt-link-part"
                       target="_blank"
                     >
-                      YouTube Link
+                      Watch Now
                     </a>
                   </div>
                 </div>
